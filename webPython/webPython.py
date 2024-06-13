b5a6e72e5d9387b90ead2a8c7b9adde56b71b3cc
Jpiamonte78 <jjpc56@gmail.com>
2024-06-13 10:40:52 -0500

    ajustes Hola mundo

--- webPython/webPython.py
+++ webPython/webPython.py
@@ -13,27 +13,8 @@ class State(rx.State):
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Hola Reflex!", size="9"),
-            rx.text(
-                "Iniciemos con el código ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
-
+    return rx.text("Hola Mundo!!",color="blue")
+   
 
 app = rx.App()
 app.add_page(index)
